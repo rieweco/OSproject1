186290e76dc3f20c8119e25c4cd48d354bf34859
Cody Riewerts <o2-riewerts@hoare7.cs.umsl.edu>
2018-02-06 10:25:03 -0600

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   fan.c

---
+++ fan.c
@@ -0,0 +1,31 @@
+//fan.c
+//Cody Riewerts
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	pid_t childpid = 0;
+	int i, n;
+	
+	if(argc != 2)
+	{
+		fprintf(stderr, "Usage: %s processes\n", argv[0]);
+		return 1;
+	}
+	
+	n = atoi(argv[1]);
+	for(i = 1; i < n; i++)
+	{
+		if((childpid = fork()) <= 0)
+		{
+			break;
+		}
+	}
+
+	fprintf(stderr, "i:%d Process ID:%1d ParentID:%1d ChildID:%1d\n",
+		 i, (long)getpid(), (long)getppid(), (long)childpid);
+
+	return 0;
+}
